f5f79ecb9524eb7557af7ed9231186958c6a5c14
Manuel Hutter <mhutter@users.noreply.github.com>
2023-08-10 10:52:13 +0200

    fix: invalid default values (#15)
    Signed-off-by: Manuel Hutter <manuel@hutter.io>
    Co-authored-by: Nick Marti <nick.marti@vshn.net>

--- internal/defaults/exoscalev1.go
+++ internal/defaults/exoscalev1.go
@@ -9,10 +9,10 @@ func (d *Defaults) GetExoscalePostgreSQLDefault() *exoscalev1.ExoscalePostgreSQL
 
 	postgreSQLdefault.Spec.Parameters.Service.MajorVersion = "15"
 	postgreSQLdefault.Spec.Parameters.Service.Zone = "ch-dk-2"
-	postgreSQLdefault.Spec.Parameters.Size.Plan = "hobbyist-2 "
+	postgreSQLdefault.Spec.Parameters.Size.Plan = "hobbyist-2"
 	postgreSQLdefault.Spec.Parameters.Backup.TimeOfDay = "12:00:00"
-	postgreSQLdefault.Spec.Parameters.Maintenance.DayOfWeek = "Sunday"
-	postgreSQLdefault.Spec.Parameters.Maintenance.TimeOfDay = "24:00:00"
+	postgreSQLdefault.Spec.Parameters.Maintenance.DayOfWeek = "sunday"
+	postgreSQLdefault.Spec.Parameters.Maintenance.TimeOfDay = "00:00:00"
 
 	return &postgreSQLdefault
 }
@@ -20,8 +20,8 @@ func (d *Defaults) GetExoscalePostgreSQLDefault() *exoscalev1.ExoscalePostgreSQL
 func (d *Defaults) GetExoscaleRedisDefault() *exoscalev1.ExoscaleRedis {
 	var redisDefault exoscalev1.ExoscaleRedis
 
-	redisDefault.Spec.Parameters.Maintenance.DayOfWeek = "Sunday"
-	redisDefault.Spec.Parameters.Maintenance.TimeOfDay = "24:00:00"
+	redisDefault.Spec.Parameters.Maintenance.DayOfWeek = "sunday"
+	redisDefault.Spec.Parameters.Maintenance.TimeOfDay = "00:00:00"
 	redisDefault.Spec.Parameters.Service.Zone = "ch-dk-2"
 
 	return &redisDefault
@@ -32,9 +32,9 @@ func (d *Defaults) GetExoscaleKafkaDefault() *exoscalev1.ExoscaleKafka {
 
 	kafkaDefault.Spec.Parameters.Service.Version = "3.4.0"
 	kafkaDefault.Spec.Parameters.Service.Zone = "ch-dk-2"
-	kafkaDefault.Spec.Parameters.Size.Plan = "startup-2 "
-	kafkaDefault.Spec.Parameters.Maintenance.DayOfWeek = "Sunday"
-	kafkaDefault.Spec.Parameters.Maintenance.TimeOfDay = "24:00:00"
+	kafkaDefault.Spec.Parameters.Size.Plan = "startup-2"
+	kafkaDefault.Spec.Parameters.Maintenance.DayOfWeek = "sunday"
+	kafkaDefault.Spec.Parameters.Maintenance.TimeOfDay = "00:00:00"
 
 	return &kafkaDefault
 }
@@ -44,10 +44,10 @@ func (d *Defaults) GetExoscaleMySQLDefault() *exoscalev1.ExoscaleMySQL {
 
 	mySQLdefault.Spec.Parameters.Service.MajorVersion = "8"
 	mySQLdefault.Spec.Parameters.Service.Zone = "ch-dk-2"
-	mySQLdefault.Spec.Parameters.Size.Plan = "hobbyist-2 "
+	mySQLdefault.Spec.Parameters.Size.Plan = "hobbyist-2"
 	mySQLdefault.Spec.Parameters.Backup.TimeOfDay = "12:00:00"
-	mySQLdefault.Spec.Parameters.Maintenance.DayOfWeek = "Sunday"
-	mySQLdefault.Spec.Parameters.Maintenance.TimeOfDay = "24:00:00"
+	mySQLdefault.Spec.Parameters.Maintenance.DayOfWeek = "sunday"
+	mySQLdefault.Spec.Parameters.Maintenance.TimeOfDay = "00:00:00"
 
 	return &mySQLdefault
 }
@@ -57,10 +57,10 @@ func (d *Defaults) GetExoscaleOpenSearchDefault() *exoscalev1.ExoscaleOpenSearch
 
 	openSearchDefault.Spec.Parameters.Service.MajorVersion = "2"
 	openSearchDefault.Spec.Parameters.Service.Zone = "ch-dk-2"
-	openSearchDefault.Spec.Parameters.Size.Plan = "hobbyist-2 "
+	openSearchDefault.Spec.Parameters.Size.Plan = "hobbyist-2"
 	openSearchDefault.Spec.Parameters.Backup.TimeOfDay = "12:00:00"
-	openSearchDefault.Spec.Parameters.Maintenance.DayOfWeek = "Sunday"
-	openSearchDefault.Spec.Parameters.Maintenance.TimeOfDay = "24:00:00"
+	openSearchDefault.Spec.Parameters.Maintenance.DayOfWeek = "sunday"
+	openSearchDefault.Spec.Parameters.Maintenance.TimeOfDay = "00:00:00"
 
 	return &openSearchDefault
 }
